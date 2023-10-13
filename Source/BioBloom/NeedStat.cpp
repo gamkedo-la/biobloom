eb4e643ac60b86cae5b118b7ccd6c1a35e2effdd
CTGabianelli11 <ctgabbianelli@gmail.com>
2023-10-13 19:33:48 -0400

    Add simple boilerplate code for stats on actors

---
+++ Source/BioBloom/NeedStat.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NeedStat.h"
+
+// Sets default values for this component's properties
+UNeedStat::UNeedStat()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+void UNeedStat::ChangeStat(float amountChanged)
+{
+	currentStat += amountChanged;
+	if (currentStat > statMax)
+		currentStat = statMax;
+
+	if (currentStat < 0)
+		currentStat = 0;
+}
+
+float UNeedStat::GetStat()
+{
+	return currentStat;
+}
+
+void UNeedStat::SetStat(float SetAmount)
+{
+	if (currentStat > statMax)
+		currentStat = statMax;
+
+	if (currentStat < 0)
+		currentStat = 0;
+}
+
+void UNeedStat::SetStatMax(float SetAmount)
+{
+	statMax = SetAmount;
+}
+
+// Called when the game starts
+void UNeedStat::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UNeedStat::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
