80ed765d49b8fc97fc60c3bc5e7cdb80bf7ee429
Tyler Funk <tcfunk25@gmail.com>
2023-10-16 12:46:41 -0500

    add test cpp component

---
+++ Source/BioBloom/PlanterLight.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlanterLight.h"
+
+
+// Sets default values for this component's properties
+UPlanterLight::UPlanterLight()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPlanterLight::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UPlanterLight::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Yellow, TEXT("Hello I'm a light"));
+	}
+}
+
