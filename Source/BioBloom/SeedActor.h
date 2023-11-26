98a31db9e7a9cd4ca1f3b7e10b8ef26b3cd9e78f
CTGabianelli11 <ctgabbianelli@gmail.com>
2023-11-26 18:42:07 -0500

    Started adding seed actors to plant actors

---
+++ Source/BioBloom/SeedActor.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SeedActor.generated.h"
+
+UCLASS()
+class BIOBLOOM_API ASeedActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASeedActor();
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Components", BlueprintReadWrite)
+	USceneComponent* defaultSceneRoot;
+
+	UPROPERTY(EditAnywhere, Instanced, Category = "Components", BlueprintReadWrite)
+	UStaticMeshComponent* seedMesh;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
